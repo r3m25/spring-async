2c8426f3621d4610c20b023099c84dfb8f5fde5e
Rubén Marcos Morales Medina <rm.morales22@gmail.com>
2020-07-25 18:09:17 -0400

    [async]: add base code

---
+++ src/main/java/com/r3m25/async/Application.java
@@ -0,0 +1,14 @@
+package com.r3m25.async;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+
+}
